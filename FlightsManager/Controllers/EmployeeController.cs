d908dd9d8cd66fb907998cf7659636a38683036f
Петя Личева <petya.licheva2002@abv.bg>
2021-03-08 00:14:02 +0200

    Some changes on design and signUp and login methods!

--- FlightsManager/Controllers/EmployeeController.cs
+++ FlightsManager/Controllers/EmployeeController.cs
@@ -31,15 +31,66 @@ namespace FlightsManager.Controllers
         {
             if (ModelState.IsValid)
             {
+                Country lastCountry = _context.Countries.Last();
+                Country countr = new Country()
+                {
+                    Id = lastCountry.Id + 1,
+                    Name = model.Country,
+                    ContinentId = model.ContinentId
+                };
+                _context.Countries.Add(countr);
+                _context.SaveChanges();
+                LivingPlace lastLivingPlace = _context.LivingPlaces.Last();
+                LivingPlace lP = new LivingPlace()
+                {
+                    Id = lastLivingPlace.Id + 1,
+                    Name = model.LivingArea
+                };
+                _context.LivingPlaces.Add(lP);
+                _context.SaveChanges();
+                DestrictedArea lastDestrictedArea = _context.DestrictedAreas.Last();
+                DestrictedArea dA = new DestrictedArea()
+                {
+                    Id = lastDestrictedArea.Id + 1,
+                    Name = model.DestrictedArea
+                };
+                _context.DestrictedAreas.Add(dA);
+                _context.SaveChanges();
+                Street lastStreet = _context.Streets.Last();
+                Street str = new Street() 
+                { 
+                    Id = lastStreet.Id + 1,
+                    Name = model.Street
+                };
+                _context.Streets.Add(str);
+                _context.SaveChanges();
+                Address lastAddress = _context.Addresses.Last();
+                Address addr = new Address()
+                {
+                    Id = lastAddress.Id + 1,
+                    ContitnetId = model.ContinentId,
+                    CountryId = countr.Id,
+                    LivingPlaceId = lP.Id,
+                    DestrictedAreaId = dA.Id,
+                    StreetId = str.Id,
+                    Number = model.Number,
+                    Floor = model.Floor,
+                    Apartament = model.Apartament,
+                    AddressTypeId = 1
+                };
+                _context.Addresses.Add(addr);
+                _context.SaveChanges();
+                Employee lastEmployee = _context.Employees.Last();
                 Employee emp = new Employee()
                 {
+                    Id = lastEmployee.Id + 1,
                     Username = model.Username,
                     Password = HashPassword.GenerateSHA512(model.Password),
                     Email = model.Email,
                     Name = model.Name,
                     Family = model.Family,
                     Egn = model.Egn,
-                    AddressId = model.AddressId,
+                    AddressId = addr.Id,
                     PhoneNumber = model.PhoneNumber,
                     RoleId = model.RoleId
                 };
@@ -84,8 +135,10 @@ namespace FlightsManager.Controllers
                     AddressId = employee.AddressId,
                     RoleId = employee.RoleId
                 };
+                IndexEmployeeViewModel indexEVM = turnEmployeeViewModelToIntexEmployeeViewModel(empViewModel);
+                return Login(indexEVM);
 
-                return RedirectToAction(nameof(Login));
+                //return RedirectToAction(nameof(Login));
             }
 
             return View(model);
@@ -95,6 +148,16 @@ namespace FlightsManager.Controllers
         {
 
             return View(model);
+        }
+
+        private IndexEmployeeViewModel turnEmployeeViewModelToIntexEmployeeViewModel(EmployeeViewModel evm) 
+        {
+            List<EmployeeViewModel> evms = new List<EmployeeViewModel>();
+            evms.Add(evm);
+            return new IndexEmployeeViewModel()
+            {
+                Items = evms
+            };
         }
     }
 }
--- FlightsManager/Models/Web/Employees/SignUpEmployeeViewModel.cs
+++ FlightsManager/Models/Web/Employees/SignUpEmployeeViewModel.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,15 +8,45 @@ namespace FlightsManager.Models.Web.Employees
 {
     public class SignUpEmployeeViewModel
     {
+        [Required]
+        [StringLength(20)]
         public string Username { get; set; }
+        [Required]
+        [MinLength(8), MaxLength(20)]
+        [RegularExpression("^(?=.*\\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z]).{8,20}$")]
         public string Password { get; set; }
+        [Required]
+        [MinLength(8), MaxLength(20)]
+        [RegularExpression("^(?=.*\\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z]).{8,20}$")]
+        [Compare(nameof(Password))]
         public string ReEnteredPassword { get; set; }
+        [Required]
         public string Email { get; set; }
+        [Required]
         public string Name { get; set; }
+        [Required]
         public string Family { get; set; }
+        [Required]
         public string Egn { get; set; }
-        public int AddressId { get; set; }
+        [Required]
+        public int ContinentId { get; set; }
+        [Required]
+        public string Country { get; set; }
+        [Required]
+        public string LivingArea { get; set; }
+        [Required]
+        public string DestrictedArea { get; set; }
+        [Required]
+        public string Street { get; set; }
+        [Required]
+        public int Number { get; set; }
+        [Required]
+        public int Floor { get; set; }
+        [Required]
+        public int Apartament { get; set; }
+        [Required]
         public string PhoneNumber { get; set; }
+        [Required]
         public int RoleId { get; set; }
     }
 }
